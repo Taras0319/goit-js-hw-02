6a38c702873afe9ee067c28e7dcfd88762626dc9
Taras0319 <143548783+Taras0319@users.noreply.github.com>
2023-11-20 21:11:18 +0200

    hm12

--- JS/task-4.js
+++ JS/task-4.js
@@ -21,6 +21,7 @@ function getShippingCost(country) {
     return `Shipping to ${country} will cost ${price} credits`;
 }
 
+// ======================================
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
